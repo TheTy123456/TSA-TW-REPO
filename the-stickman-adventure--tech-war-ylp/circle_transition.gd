da3db71d1272eb63b6bcfe3229b45eb1b725a548
TheTy123456 <tylandisbly@gmail.com>
2026-09-14 09:11:45 -0400

    Changes to platformer

--- the-stickman-adventure--tech-war-ylp/circle_transition.gd
+++ the-stickman-adventure--tech-war-ylp/circle_transition.gd
@@ -1,5 +1,5 @@
 extends CanvasLayer
-@onready var animation_player: AnimationPlayer = $ColorRect/AnimationPlayer
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func play_circle_fade_in():
 	animation_player.play("circle_fade_out")
